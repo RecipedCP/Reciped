b178a9942c1fbe96ad04134dcba892b45f4b87b2
Alex Espinoza-Fuentes <alexespinoza11f@gmail.com>
2020-11-19 19:32:23 -0800

    pushing filed

--- app/src/main/java/com/example/recipedcode/SignUpActivity.java
+++ app/src/main/java/com/example/recipedcode/SignUpActivity.java
@@ -9,6 +9,6 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("tghis is signup");
+        System.out.println("this is signup");
     }
 }
